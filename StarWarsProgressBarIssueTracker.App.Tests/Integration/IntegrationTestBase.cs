699dcbfd6fc2ba5daa0c4c5857b0ebfd1e1884be
Christopher Osthues <christopher.osthues@gmail.com>
2023-11-25 22:35:27 +0100

    feat: Setup integration tests

---
+++ StarWarsProgressBarIssueTracker.App.Tests/Integration/IntegrationTestBase.cs
@@ -0,0 +1,70 @@
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using StarWarsProgressBarIssueTracker.App.Tests.Integration.Setup;
+using StarWarsProgressBarIssueTracker.Infrastructure;
+
+namespace StarWarsProgressBarIssueTracker.App.Tests.Integration;
+
+/// <summary>
+/// Base class for all integration tests.
+/// </summary>
+public class IntegrationTestBase
+{
+    protected IssueTrackerWebApplicationFactory ApiFactory = default!;
+    protected HttpClient HttpClient = default!;
+
+    [OneTimeSetUp]
+    public void SetUpOnceBase()
+    {
+        ApiFactory = new IssueTrackerWebApplicationFactory();
+        HttpClient = ApiFactory.CreateClient(new WebApplicationFactoryClientOptions { AllowAutoRedirect = false });
+    }
+
+    [SetUp]
+    public void SetUpBase()
+    {
+        using var scope = ApiFactory.GetServiceProvider().CreateScope();
+        var serviceProvider = scope.ServiceProvider;
+        var dbContext = serviceProvider.GetRequiredService<IssueTrackerContext>();
+        SeedDatabase(dbContext);
+    }
+
+    /// <summary>
+    /// This method seeds the database before each test. This is the place where you can provide test specific data
+    /// with which the database is seeded. The default implementation seeds the database with some mocked data.
+    /// </summary>
+    /// <param name="dbContext">The database context used to seed the database</param>
+    protected virtual void SeedDatabase(IssueTrackerContext dbContext)
+    {
+        // TODO: seed db with data
+    }
+
+    [TearDown]
+    public void TearDownBase()
+    {
+        using var scope = ApiFactory.GetServiceProvider().CreateScope();
+        var serviceProvider = scope.ServiceProvider;
+        var dbContext = serviceProvider.GetRequiredService<IssueTrackerContext>();
+        ResetDatabase(dbContext);
+    }
+
+    /// <summary>
+    /// This method reset the database after each test. This is the place where you can clear the database. The default
+    /// implementation deletes all data from the database.
+    /// </summary>
+    /// <param name="dbContext">The database context used to reset the database</param>
+    protected virtual void ResetDatabase(IssueTrackerContext dbContext)
+    {
+        dbContext.Issues.RemoveRange(dbContext.Issues);
+        dbContext.Milestones.RemoveRange(dbContext.Milestones);
+        dbContext.Labels.RemoveRange(dbContext.Labels);
+        dbContext.Releases.RemoveRange(dbContext.Releases);
+    }
+
+    [OneTimeTearDown]
+    public async Task TearDownOnceBase()
+    {
+        HttpClient.Dispose();
+        await ApiFactory.DisposeAsync();
+    }
+}
---
+++ StarWarsProgressBarIssueTracker.App.Tests/Integration/Setup/ReplaceDbContextExtensions.cs
@@ -0,0 +1,28 @@
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using StarWarsProgressBarIssueTracker.Infrastructure;
+
+namespace StarWarsProgressBarIssueTracker.App.Tests.Integration.Setup;
+
+public static class ReplaceDbContextExtensions
+{
+    public static void ReplaceDbContext(this IServiceCollection services)
+    {
+        var dbContextDescriptor =
+            services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<IssueTrackerContext>));
+        if (dbContextDescriptor is not null)
+        {
+            services.Remove(dbContextDescriptor);
+        }
+
+        // Create open SqliteConnection so EF won't automatically close it.
+        var connection = new SqliteConnection("DataSource=:memory:");
+        connection.Open();
+
+        services.AddDbContext<IssueTrackerContext>((container, options) =>
+        {
+            options.UseSqlite(connection);
+        });
+    }
+}
