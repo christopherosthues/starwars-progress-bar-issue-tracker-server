c4836c6e3fa3ad69275714bd4b99b9731c602e63
Christopher Osthues <christopher.osthues@gmail.com>
2023-11-25 21:52:37 +0100

    refactor: Adjust .editorconfig; remove whitespace

--- StarWarsProgressBarIssueTracker.Infrastructure/Models/DbLabel.cs
+++ StarWarsProgressBarIssueTracker.Infrastructure/Models/DbLabel.cs
@@ -3,10 +3,10 @@
 public class DbLabel : DbEntity
 {
     public required string Title { get; set; }
-    
+
     public string? Description { get; set; }
-    
+
     public required string Color { get; set; }
-    
+
     public required string TextColor { get; set; }
 }
