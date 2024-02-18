26fa103571412e24bd6891054042701d5d2bf29c
Christopher Osthues <christopher.osthues@gmail.com>
2024-02-18 13:49:20 +0100

    chore: Remove GitHub project

--- StarWarsProgressBarIssueTracker.Infrastructure.Github/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using Microsoft.Extensions.DependencyInjection;
-
-Console.WriteLine("Hello, World!");
-
-var serviceCollection = new ServiceCollection();
-serviceCollection.AddGitHubClient();
